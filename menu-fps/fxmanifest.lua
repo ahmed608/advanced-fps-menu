7a7073569fc0639706c816d87b379cc2f49331d0
konarPlus <57233235+ahmed608@users.noreply.github.com>
2022-08-26 17:49:42 +0200

    Add files via upload

---
+++ menu-fps/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+name "fps-menu"
+description "fps-menu standlone"
+author 'Egypt Studio & kh dev team'
+
+ui_page "index.html"
+
+client_script "client.lua"
+server_script "server.lua"
+
+files {
+    "index.html",
+    "style.css",
+    "main.js",
+    "img/*",
+}
